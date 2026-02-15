01ce87b7427649d4c4013ee3cd034740347e0628
Cerallin <cerallin@cerallin.top>
2026-02-15 16:14:43 +0000

    feat(debug): Implement debug initialization and logging functions

--- include/common.h
+++ include/common.h
@@ -6,7 +6,10 @@
  */
 #pragma once
 
+#include <cmath>
+#include <cstdarg>
 #include <cstddef>
+#include <cstdio>
 #include <cstdlib>
 #include <cstring>
 
@@ -15,3 +18,20 @@
 #if (_LIBNDS_MAJOR_ == 2)
 #error "This project is not compatable to libnds v2.0.0+!"
 #endif
+
+static inline void
+debugInit(void) {
+    consoleDebugInit(DebugDevice_NOCASH);
+}
+
+static inline int
+debugf(const char *fmt, ...) {
+    int result = 0;
+#ifndef NDEBUG
+    va_list args;
+    va_start(args, fmt);
+    result = vfprintf(stderr, fmt, args);
+    va_end(args);
+#endif
+    return result;
+}
