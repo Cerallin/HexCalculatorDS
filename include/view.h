6666637958d5d69bcb4b10448af34e9995e25947
Cerallin <cerallin@cerallin.top>
2026-02-17 13:30:29 +0000

    refactor(view): Update Area struct to use int16_t and uint8_t types

--- include/view.h
+++ include/view.h
@@ -12,41 +12,55 @@
 
 namespace HexCalc {
 
+/**
+ * @brief The alignment of the view text.
+ *
+ */
 enum ViewAlign : uint8_t {
     AlignLeft,
     AlignRight,
 };
 
+/**
+ * @brief The area of a view. all values are int16_t, is enough for the current
+ * screen resolution (256x192).
+ *
+ */
 struct Area {
-    /** position x */
-    int x;
-    /** position y */
-    int y;
-
-    /** area width */
-    int w;
-    /** area height */
-    int h;
+    /** position x, can be negative */
+    int16_t x;
+    /** position y, can be negative */
+    int16_t y;
+    /** area width, must > 0 */
+    uint8_t w;
+    /** area height, must > 0 */
+    uint8_t h;
 
     static Area
-    AreaByPoints(int x1, int x2, int y1, int y2) {
-        int x_min = std::min(x1, x2);
-        int x_max = std::max(x1, x2);
-        int y_min = std::min(y1, y2);
-        int y_max = std::max(y1, y2);
+    AreaByPoints(int16_t x1, int16_t x2, int16_t y1, int16_t y2) {
+        auto x_min = std::min(x1, x2);
+        auto x_max = std::max(x1, x2);
+        auto y_min = std::min(y1, y2);
+        auto y_max = std::max(y1, y2);
 
-        Area area(x_min, y_min, (x_max - x_min), (y_max - y_min));
+        // The area width and height must be less than 256, otherwise it cannot
+        // be represented by uint8_t.
+        assert(x_max - x_min <= std::numeric_limits<uint8_t>::max());
+        assert(y_max - y_min <= std::numeric_limits<uint8_t>::max());
+
+        auto width = static_cast<uint8_t>(x_max - x_min);
+        auto height = static_cast<uint8_t>(y_max - y_min);
+
+        Area area(x_min, y_min, width, height);
 
         return area;
     }
 
-    Area(int x, int y, int w, int h) {
-        assert((w > 0) && (h > 0));
-
-        x = x;
-        y = y;
-        w = w;
-        h = h;
+    Area(int16_t x, int16_t y, uint8_t w, uint8_t h) {
+        this->x = x;
+        this->y = y;
+        this->w = w;
+        this->h = h;
     }
 };
 
