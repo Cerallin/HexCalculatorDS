3f94bd7f0086362b43d9b4c3cc847c4dc82277de
Cerallin <cerallin@cerallin.top>
2026-02-25 07:33:26 +0000

    feat(model): Enhance operator handling in formula evaluation process

--- source/model.cc
+++ source/model.cc
@@ -36,8 +36,11 @@ FormulaModel::HandleEvent(const Event &e) {
         } else {
             currentNumber = NumberZero;
         }
+
         formulaTree.Clear();
+        formulaTree.Input(FormulaData(currentNumber));
         inputState = PlaceHolder;
+
         formulaChanged = true;
         valueChanged = true;
     } else if (e.type == UpdateBaseEvent) {
@@ -89,35 +92,42 @@ FormulaModel::handleInput(const Event &e) {
 
     if (eventData.isOp) { // is operator
         OperatorType op = eventData.data.op;
-        // 1. insert current number into formula tree
-        debugf("input number: %llu\n", currentNumber);
-        formulaTree.Input(FormulaData(currentNumber));
-        // 2. evaluate the formula tree
-        auto lastOp = formulaTree.LastOperator();
-        if (Operator::HigherThan(op, lastOp)) {
-            // do not evaluate if current operator has higher precedence than
-            // the previous one, otherwise it will cause wrong result for
-            // expressions like "1 + 2 * 3"
-        } else { // op <= lastOp
-            bool evaluated = formulaTree.EvaluatePartial();
-            debugf("Partial evaluation result: %s\n",
-                   evaluated ? "OK" : "Error");
-            if (evaluated) {
-                currentNumber = formulaTree.Result();
-                valueChanged = true;
-            } else {
-                // do not update current number if evaluation failed
+        if (inputState == PlaceHolder) {
+            // try to insert operator
+            formulaTree.Input(FormulaData(op));
+            formulaChanged = true;
+        } else {
+            // 1. insert current number into formula tree
+            debugf("input number: %llu\n", currentNumber);
+            formulaTree.Input(FormulaData(currentNumber));
+            // 2. evaluate the formula tree
+            auto lastOp = formulaTree.LastOperator();
+            if (Operator::HigherThan(op, lastOp)) {
+                // do not evaluate if current operator has higher precedence
+                // than the previous one, otherwise it will cause wrong result
+                // for expressions like "1 + 2 * 3"
+            } else { // op <= lastOp
+                bool evaluated = formulaTree.EvaluatePartial();
+                debugf("Partial evaluation result: %s\n",
+                       evaluated ? "OK" : "Error");
+                if (evaluated) {
+                    currentNumber = formulaTree.Result();
+                    valueChanged = true;
+                } else {
+                    // do not update current number if evaluation failed
+                }
             }
+            // 3. insert operator into formula tree
+            // must after evaluation, otherwise the new operator will affect the
+            // evaluation
+            debugf("input op: %d\n", op);
+            formulaTree.Input(FormulaData(op));
+            formulaChanged = true;
+            // 4. Mark the current number as placeholder, so that the next digit
+            // input will start a new number instead of joining the current
+            // number.
+            inputState = PlaceHolder;
         }
-        // 3. insert operator into formula tree
-        // must after evaluation, otherwise the new operator will affect the
-        // evaluation
-        debugf("input op: %d\n", op);
-        formulaTree.Input(FormulaData(op));
-        formulaChanged = true;
-        // 4. Mark the current number as placeholder, so that the next digit
-        // input will start a new number instead of joining the current number.
-        inputState = PlaceHolder;
     } else { // is number
         if (inputState == PlaceHolder) {
             // if the previous input is an operator, start a new number
