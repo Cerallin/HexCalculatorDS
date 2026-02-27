0bb0cf1dd5783ce09620e9e72d23011934efa75a
Cerallin <cerallin@cerallin.top>
2026-02-27 07:26:12 +0000

    chore: Add missing header documentation to config.cc

--- source/config.cc
+++ source/config.cc
@@ -1,3 +1,9 @@
+/**
+ * HexCalculatorDS - An integer calculator for Nintendo DS.
+ * Copyright (C) 2026  Cerallin <cerallin@cerallin.top>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
 #include "config.h"
 
 using namespace HexCalc;
